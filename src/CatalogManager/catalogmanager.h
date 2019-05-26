7d129e80f0389831fe7ce0478a5c34a2020f462d
11gan6 <50435482+11gan6@users.noreply.github.com>
2019-05-26 16:55:35 +0800

    catalogmanager.h 初稿
    可能还需删改

---
+++ src/CatalogManager/catalogmanager.h
@@ -0,0 +1,76 @@
+//
+//	catalogmanager.h
+//	目录管理员
+
+#ifndef _CATALOGMANAGER_H_
+#define _CATALOGMANAGER_H_ 1
+
+#include <iostream>
+#include <math>
+#include <string>
+#include <vector>
+#include "../base.h" 
+using namespace std;
+
+class Catalog{
+	private:
+		//把表格信息写入写出文件时需要用到转换的函数 
+		int String2Num(string tmp);
+		string Num2String(int tmp);
+		
+		//得到存放某表格信息的块数 
+		int GetBlockAmount(string tablename);
+		
+	public:
+		//关于表格的操作 
+		
+		//创建表格
+		//输入：表格名称、表格属性、索引对象、主码 
+		//输出: 1-成功； 0-失败,包含异常 
+		int CreateTable(string tablename, Attribute attr, Index indices, int primary_key);
+		
+		//删除表格
+		//输入：表格名称
+		//输出：1-成功； 0-失败,包含异常  
+		int DropTable(string tablename);
+		 
+		//通过表名查看表是否存在	
+		//输入：表格名称
+		//输出：true-存在； false-不存在
+		bool isTableExist(string tablename);
+		
+		//打印表格信息  ??待定，不知道查询结果的反馈方式 
+		void PrintTable(string tablename, Attribute tattr); 
+	
+	
+		//关于属性和索引
+		
+		//某一属性是否存在
+		//输入：表格名称、属性名称
+		//输出：true-存在； false-不存在		 
+		bool isAttributeExist(string tablename, string tattr);
+		
+		//得到某表的全部属性
+		//输入：表格名称
+		//输出：Attribute结构数据
+		Attribute GetTableAttribute(string tablename);
+		
+		//在指定属性上建立索引
+		//输入：表格名称、属性名称、索引名称
+		//输出：1-成功； 0-失败,包含异常
+		int CreateIndex(string tablename, string tattr, string indexname);
+		
+		//删除索引
+		//输入：表格名称、索引名称
+		//输出：1-成功； 0-失败,包含异常
+		int DropIndex(string tablename, string indexname);
+		
+		//索引是否存在
+		//输入：表格名称、索引名称
+		//输出：true-存在； false-不存在		 
+		bool isIndexExist(string tablename, string indexname);
+		 
+};
+
+#endif 
+ 

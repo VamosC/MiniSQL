6c011ef0c5e4f72f37317e857e42c7aaeed3d1cd
VamosC <3160102243@zju.edu.cn>
2019-06-06 21:31:53 +0800

    finish bplustree insert and delete

--- src/IndexManager/bplustree.h
+++ src/IndexManager/bplustree.h
@@ -18,6 +18,7 @@ inline int round_2(int degree)
 	return degree % 2 == 0 ? degree/2 : (degree/2) + 1;
 }
 
+// 转换成json方便调试
 template <typename T>
 inline std::string to_string(std::vector<T> &arr)
 {
@@ -128,11 +129,12 @@ template <typename T>
 class BPTree
 {
 public:
-	BPTree() : degree(4){}
-	bool _find(const T& key, NodeFound<T> &res);// 查询key的所在的节点
-	bool _insert(const T& key, int block_id);// 插入索引结点
-	bool _delete(const T& key);
-	void print()
+	BPTree() {}
+	bool _find(const T &key, NodeFound<T> &res);// 查询key的所在的节点
+	bool _insert(const T &key, int block_id);// 插入索引结点
+	bool _delete(const T &key);// 删除索引结点
+	bool _find_range(const T& start, const T& end, std::vector<int> &res);// 范围查找
+	void print() // for debug
 	{
 		root->print();
 	}
@@ -140,10 +142,13 @@ private:
 	std::shared_ptr<TreeNode<T>> root;
 	int degree;
 	void insert_in_parent(std::shared_ptr<TreeNode<T>> &l_node, const T& key, std::shared_ptr<TreeNode<T>> &r_node);
+	void adjust_in_parent(std::shared_ptr<TreeNode<T>> &node);
+	void left_rotate(std::shared_ptr<TreeNode<T>> &l_node, std::shared_ptr<TreeNode<T>> &r_node);
+	void right_rotate(std::shared_ptr<TreeNode<T>> &l_node, std::shared_ptr<TreeNode<T>> &r_node);
 };
 
 template <typename T>
-bool BPTree<T>::_find(const T& key, NodeFound<T> &res)
+bool BPTree<T>::_find(const T &key, NodeFound<T> &res)
 {
 	// 根结点为空
 	if(root == nullptr)
@@ -204,7 +209,7 @@ bool BPTree<T>::_find(const T& key, NodeFound<T> &res)
 }
 
 template <typename T>
-bool BPTree<T>::_insert(const T& key, int block_id)
+bool BPTree<T>::_insert(const T &key, int block_id)
 {
 	// 根结点为空
 	if(root == nullptr)
@@ -252,7 +257,9 @@ bool BPTree<T>::_insert(const T& key, int block_id)
 
 				// 右兄弟非空
 				if(new_node->next_sibling != nullptr)
+				{
 					new_node->next_sibling->left_sibling = new_node;
+				}
 
 				new_node->num = degree - n_2;
 				node->num = n_2;
@@ -281,7 +288,7 @@ bool BPTree<T>::_insert(const T& key, int block_id)
 }
 
 template <typename T>
-void BPTree<T>::insert_in_parent(std::shared_ptr<TreeNode<T>> &l_node, const T& key, std::shared_ptr<TreeNode<T>> &r_node)
+void BPTree<T>::insert_in_parent(std::shared_ptr<TreeNode<T>> &l_node, const T &key, std::shared_ptr<TreeNode<T>> &r_node)
 {
 	// 根结点
 	if(l_node->parent == nullptr)
@@ -307,6 +314,7 @@ void BPTree<T>::insert_in_parent(std::shared_ptr<TreeNode<T>> &l_node, const T& 
 			//split
 			auto new_node = std::make_shared<TreeNode<T>>();
 			auto n_2 = round_2(degree);
+			new_node->parent = parent_node->parent;
 			parent_node->add(key, r_node);
 			auto ret_key = parent_node->keys[n_2-1];
 			for(auto i = n_2; i < parent_node->num-1;i++)
@@ -329,7 +337,9 @@ void BPTree<T>::insert_in_parent(std::shared_ptr<TreeNode<T>> &l_node, const T& 
 			parent_node->next_sibling = new_node;
 			new_node->left_sibling = parent_node;
 			if(new_node->next_sibling != nullptr)
+			{
 				new_node->next_sibling->left_sibling = new_node;
+			}
 			insert_in_parent(parent_node, ret_key, new_node);
 		}
 
@@ -342,7 +352,7 @@ void BPTree<T>::insert_in_parent(std::shared_ptr<TreeNode<T>> &l_node, const T& 
 }
 
 template <typename T>
-bool BPTree<T>::_delete(const T& key)
+bool BPTree<T>::_delete(const T &key)
 {
 	// 根结点为空
 	if(root == nullptr)
@@ -362,12 +372,15 @@ bool BPTree<T>::_delete(const T& key)
 		else
 		{
 			auto node = res.node;
+
+			// 先删
+			node->keys.erase(node->keys.begin()+res.pos);
+			node->block_ids.erase(node->block_ids.begin()+res.pos);
+			node->num--;
+
 			// 删除key后仍然half-full 直接删除
-			if(node->num - 1 >= round_2(degree-1))
+			if(node->num >= round_2(degree-1))
 			{
-				node->keys.erase(node->keys.begin()+res.pos);
-				node->block_ids.erase(node->block_ids.begin()+res.pos);
-				node->num--;
 				return true;
 			}
 
@@ -377,9 +390,6 @@ bool BPTree<T>::_delete(const T& key)
 				// 首先查看是否为根结点, special case
 				if(node->parent == nullptr)
 				{
-					node->keys.erase(node->keys.begin()+res.pos);
-					node->block_ids.erase(node->block_ids.begin()+res.pos);
-					node->num--;
 					return true;
 				}
 
@@ -391,15 +401,12 @@ bool BPTree<T>::_delete(const T& key)
 					// 可借
 					if(node->left_sibling->num - 1 >= round_2(degree-1))
 					{
-						// 先删
-						node->keys.erase(node->keys.begin()+res.pos);
-						node->block_ids.erase(node->block_ids.begin()+res.pos);
-
 						// 借左兄弟最后一个key
 						auto l_bro = node->left_sibling;
 						auto key_tmp = l_bro->keys.back();
 						node->keys.insert(node->keys.begin(), l_bro->keys.back());
 						node->block_ids.insert(node->block_ids.begin(), l_bro->keys.back());
+						node->num++;
 
 						// 抹掉左兄弟的key
 						l_bro->num--;
@@ -417,7 +424,9 @@ bool BPTree<T>::_delete(const T& key)
 						for(i = 0; i < parent_node->children.size(); i++)
 						{
 							if(parent_node->children[i] == node)
+							{
 								break;
+							}
 						}
 						// 修改内部结点的索引值
 						parent_node->keys[i-1] = key_tmp;
@@ -431,25 +440,116 @@ bool BPTree<T>::_delete(const T& key)
 							// 借得到key
 							if(node->next_sibling->num - 1 >= round_2(degree-1))
 							{
-								// 先删
-								node->keys.erase(node->keys.begin()+res.pos);
-								node->block_ids.erase(node->block_ids.begin()+res.pos);
-
 								// 借右边兄弟的第一个key
 								auto r_bro = node->next_sibling;
 								node->keys.push_back(r_bro->keys.front());
 								node->block_ids.push_back(r_bro->block_ids.front());
+								node->num++;
 
 								// 抹掉右兄弟的第一个结点
 								r_bro->keys.erase(r_bro->keys.begin());
 								r_bro->block_ids.erase(r_bro->block_ids.begin());
 								r_bro->num--;
-								// 无需更新父结点
+
+								auto key_tmp = r_bro->keys.front();
+
+								// 更新父结点
+								auto parent_node = r_bro->parent;
+								node = r_bro;
+								while(parent_node->children[0] == node)
+								{
+									node = parent_node;
+									parent_node = parent_node->parent;
+								}
+
+								auto i = 0;
+								for(i = 0; i < parent_node->children.size(); i++)
+								{
+									if(parent_node->children[i] == node)
+									{
+										break;
+									}
+								}
+								parent_node->keys[i-1] = key_tmp;
 								return true;
 							}
+							// 右兄弟借不到, 与相同父母的兄弟merge
+							else
+							{
+								// 和左结点父母相同
+								if(node->parent == node->left_sibling->parent)
+								{	
+									// 复制
+									auto l_bro = node->left_sibling;
+									for(auto i = 0; i < node->keys.size(); i++)
+									{
+										l_bro->keys.push_back(node->keys[i]);
+										l_bro->block_ids.push_back(node->block_ids[i]);
+										l_bro->num++;
+									}
+
+									// 调整指针关系
+									l_bro->next_sibling = node->next_sibling;
+									node->next_sibling->left_sibling = l_bro;
+
+									// 调整父结点
+									adjust_in_parent(node);
+
+									return true;
+								}
+
+								// 和右结点父母相同
+								else if(node->parent == node->next_sibling->parent)
+								{
+									auto r_bro = node->next_sibling;
+									for(auto i = 0;i < r_bro->keys.size(); i++)
+									{
+										node->keys.push_back(r_bro->keys[i]);
+										node->block_ids.push_back(r_bro->block_ids[i]);
+										node->num++;
+									}
+
+									// 调整指针关系
+									node->next_sibling = r_bro->next_sibling;
+									if(r_bro->next_sibling != nullptr)
+									{
+										r_bro->next_sibling->left_sibling = node;
+									}
+									// 调整父结点
+									adjust_in_parent(r_bro);
+
+									return true;
+								}
+
+								// 不可能存在单个结点没有相同父母的兄弟
+								else
+								{
+									assert(false);
+								}
+							}
 						}
-						// 右兄弟不存在或者借不到, 那么和左兄弟进行合并操作
-						return true;
+
+						// 右兄弟不存在, 那么和左兄弟进行合并操作
+						else
+						{
+							assert(node->parent == node->left_sibling->parent);
+							auto l_bro = node->left_sibling;
+							for(auto i = 0; i < node->keys.size(); i++)
+							{
+								l_bro->keys.push_back(node->keys[i]);
+								l_bro->block_ids.push_back(node->block_ids[i]);
+								l_bro->num++;
+							}
+
+							// 调整指针关系
+							// 右兄弟不存在
+							l_bro->next_sibling = node->next_sibling;
+
+							// 调整父结点
+							adjust_in_parent(node);
+							return true;
+						}
+
 					}
 				}
 
@@ -459,33 +559,404 @@ bool BPTree<T>::_delete(const T& key)
 					// 借得到key
 					if(node->next_sibling->num - 1 >= round_2(degree-1))
 					{
-						// 先删
-						node->keys.erase(node->keys.begin()+res.pos);
-						node->block_ids.erase(node->block_ids.begin()+res.pos);
-
 						// 借右边兄弟的第一个key
 						auto r_bro = node->next_sibling;
 						node->keys.push_back(r_bro->keys.front());
 						node->block_ids.push_back(r_bro->block_ids.front());
+						node->num++;
 
 						// 抹掉右兄弟的第一个结点
 						r_bro->keys.erase(r_bro->keys.begin());
 						r_bro->block_ids.erase(r_bro->block_ids.begin());
 						r_bro->num--;
-						// 无需更新父结点
+
+						auto key_tmp = r_bro->keys.front();
+						// 更新父结点
+						auto parent_node = r_bro->parent;
+						node = r_bro;
+						while(parent_node->children[0] == node)
+						{
+							node = parent_node;
+							parent_node = parent_node->parent;
+						}
+
+						auto i = 0;
+						for(i = 0; i < parent_node->children.size(); i++)
+						{
+							if(parent_node->children[i] == node)
+							{
+								break;
+							}
+						}
+						parent_node->keys[i-1] = key_tmp;
+
 						return true;
 					}
 					// 借不到key, 和右兄弟进行合并
 					else
 					{
+						assert(node->parent == node->next_sibling->parent);
+						auto r_bro = node->next_sibling;
+						for(auto i = 0;i < r_bro->keys.size(); i++)
+						{
+							node->keys.push_back(r_bro->keys[i]);
+							node->block_ids.push_back(r_bro->block_ids[i]);
+							node->num++;
+						}
+
+						// 调整指针关系
+						node->next_sibling = r_bro->next_sibling;
+						if(r_bro->next_sibling != nullptr)
+						{
+							r_bro->next_sibling->left_sibling = node;
+						}
+
+						// 调整父结点
+						adjust_in_parent(r_bro);
+
 						return true;
 					}
 				}
 				// 不可能既没有左兄弟又没有右兄弟
 				// 根结点的情况已经检查过
 				else
+				{
 					assert(false);
+				}
 			}
+			return true;
 		}
 	}
+}
+
+template <typename T>
+void BPTree<T>::adjust_in_parent(std::shared_ptr<TreeNode<T>> &node)
+{
+	// node不可能是根结点
+	// 根结点的情况已经处理过
+	// 说明parent一定是存在的
+
+	auto parent_node = node->parent;
+	assert(parent_node != nullptr);
+
+	// 找到要删的结点
+	auto i = 0;
+	for(i = 0; i < parent_node->children.size(); i++)
+	{
+		if(parent_node->children[i] == node)
+			break;
+	}
+	assert(i >= 0 && i < parent_node->children.size());
+	// 先删该结点中的内容
+	parent_node->children.erase(parent_node->children.begin()+i);
+	parent_node->keys.erase(parent_node->keys.begin()+i-1);
+	parent_node->num--;
+
+	// 父结点是根结点 special case
+	if(parent_node->parent == nullptr)
+	{
+		// 根结点没有key 应当删除该根结点
+		if(parent_node->num == 1)
+		{
+			root = parent_node->children[0];
+			root->parent = nullptr;
+		}
+		return;
+	}
+
+	// 检查当前结点是否half-full
+	// 已经half-full则不需要调整
+	// 否则需要进行调整
+	if(parent_node->num < round_2(degree))
+	{
+		// 首先检查能否借
+		if(parent_node->left_sibling != nullptr)
+		{
+			auto l_bro = parent_node->left_sibling;
+			// 左兄弟可借
+			if(l_bro->num-1 >= round_2(degree))
+			{
+				right_rotate(l_bro, parent_node);
+			}
+
+			// 左兄弟不可借
+			else
+			{
+				// 向右兄弟借
+				if(parent_node->next_sibling != nullptr)
+				{
+					auto r_bro = parent_node->next_sibling;
+					// 右兄弟可借
+					if(r_bro->num-1 >= round_2(degree))
+					{
+						left_rotate(parent_node, r_bro);
+					}
+				    // 右兄弟不可借
+					else
+					{
+						// 与右兄弟是一个父亲
+						if(parent_node->parent == r_bro->parent)
+						{
+							auto p = parent_node->parent;
+							auto i = 0;
+
+							// 查找对应的索引key
+							for(i = 0; i < p->children.size(); i++)
+							{
+								if(p->children[i] == parent_node)
+								{
+									break;
+								}
+							}
+							// 将右兄弟内容合并到结点中
+							parent_node->keys.push_back(p->keys[i]);
+
+							for(auto i = 0; i < r_bro->keys.size(); i++)
+							{
+								parent_node->keys.push_back(r_bro->keys[i]);
+								parent_node->children.push_back(r_bro->children[i]);
+								parent_node->num++;
+
+								// 调整父指针
+								r_bro->children[i]->parent = parent_node;
+							}
+							parent_node->children.push_back(r_bro->children[r_bro->children.size()-1]);
+							parent_node->num++;
+							r_bro->children[r_bro->children.size()-1]->parent = parent_node;
+							
+							// 调整指针关系
+							parent_node->next_sibling = r_bro->next_sibling;
+							if(r_bro->next_sibling != nullptr)
+							{
+								r_bro->next_sibling->left_sibling = parent_node;
+							}
+							adjust_in_parent(r_bro);
+						}
+
+						// 与左兄弟是一个父亲
+						else if(parent_node->parent == l_bro->parent)
+						{
+							auto p = parent_node->parent;
+							auto i = 0;
+							for(i = 0; i < p->children.size(); i++)
+							{
+								if(p->children[i] == parent_node)
+								{
+									break;
+								}
+							}
+							l_bro->keys.push_back(p->keys[i-1]);
+
+							// 合并右兄弟到左兄弟中
+							for(auto i = 0; i < parent_node->keys.size(); i++)
+							{
+								l_bro->keys.push_back(parent_node->keys[i]);
+								l_bro->children.push_back(parent_node->children[i]);
+
+								// 调整结点大小
+								l_bro->num++;
+
+								// 调整父指针
+								parent_node->children[i]->parent = l_bro;
+							}
+							l_bro->children.push_back(parent_node->children[parent_node->children.size()-1]);
+							l_bro->num++;
+							l_bro->children[l_bro->children.size()-1]->parent = l_bro;
+
+							// 调整指针关系
+							l_bro->next_sibling = parent_node->next_sibling;
+							if(parent_node->next_sibling)
+							{
+								parent_node->next_sibling->left_sibling = l_bro;
+							}
+
+							adjust_in_parent(parent_node);
+						}
+						else
+						{
+							assert(false);
+						}
+					}
+				}
+				// 右兄弟不存在
+				// 与左兄弟合并
+				else
+				{
+					assert(l_bro->parent == parent_node->parent);
+					auto p = parent_node->parent;
+					auto i = 0;
+					for(i = 0; i < p->children.size(); i++)
+					{
+						if(p->children[i] == parent_node)
+						{
+							break;
+						}
+					}
+					l_bro->keys.push_back(p->keys[i-1]);
+
+					// 合并右兄弟到左兄弟中
+					for(auto i = 0; i < parent_node->keys.size(); i++)
+					{
+						l_bro->keys.push_back(parent_node->keys[i]);
+						l_bro->children.push_back(parent_node->children[i]);
+
+						// 调整结点大小
+						l_bro->num++;
+
+						// 调整父指针
+						parent_node->children[i]->parent = l_bro;
+					}
+					l_bro->children.push_back(parent_node->children[parent_node->children.size()-1]);
+					l_bro->num++;
+					l_bro->children[l_bro->children.size()-1]->parent = l_bro;
+
+					// 调整指针关系
+					l_bro->next_sibling = parent_node->next_sibling;
+					if(parent_node->next_sibling)
+					{
+						parent_node->next_sibling->left_sibling = l_bro;
+					}
+
+					adjust_in_parent(parent_node);
+				}
+			}
+		}
+		else if(parent_node->next_sibling != nullptr)
+		{
+			auto r_bro = parent_node->next_sibling;
+
+			// 右兄弟可借
+			if(r_bro->num-1 >= round_2(degree))
+			{
+				left_rotate(parent_node, r_bro);
+			}
+
+			// 右兄弟不可借
+			// 与右兄弟合并
+			// 与右兄弟一定是相同父亲
+			else
+			{
+				auto p = parent_node->parent;
+				auto i = 0;
+
+				// 查找对应的索引key
+				for(i = 0; i < p->children.size(); i++)
+				{
+					if(p->children[i] == parent_node)
+					{
+						break;
+					}
+				}
+				// 将右兄弟内容合并到结点中
+				parent_node->keys.push_back(p->keys[i]);
+
+				for(auto i = 0; i < r_bro->keys.size(); i++)
+				{
+					parent_node->keys.push_back(r_bro->keys[i]);
+					parent_node->children.push_back(r_bro->children[i]);
+					parent_node->num++;
+					// 调整父指针
+					r_bro->children[i]->parent = parent_node;
+				}
+				parent_node->children.push_back(r_bro->children[r_bro->children.size()-1]);
+				parent_node->num++;
+				r_bro->children[r_bro->children.size()-1]->parent = parent_node;
+							
+				// 调整指针关系
+				parent_node->next_sibling = r_bro->next_sibling;
+				if(r_bro->next_sibling != nullptr)
+				{
+					r_bro->next_sibling->left_sibling = parent_node;
+				}
+				adjust_in_parent(r_bro);
+			}
+		}
+		else
+			assert(false);
+	}
+}
+
+template <typename T>
+void BPTree<T>::left_rotate(std::shared_ptr<TreeNode<T>> &l_node, std::shared_ptr<TreeNode<T>> &r_node)
+{
+	// 查找相同的父母
+	auto p = r_node;
+	auto common_parent = p->parent;
+	while(common_parent->children[0] == p)
+	{
+		p = common_parent;
+		common_parent = common_parent->parent;
+	}
+
+	auto i = 0;
+	for(i = 0; i < common_parent->children.size(); i++)
+	{
+		if(common_parent->children[i] == p)
+		{
+			break;
+		}
+	}
+
+	auto key_tmp = common_parent->keys[i-1];
+
+	// 将借到的兄弟插入
+	l_node->children.push_back(r_node->children.front());
+	l_node->keys.push_back(key_tmp);
+
+	// 调整父指针
+	l_node->children[l_node->children.size()-1]->parent = l_node;
+
+	// 调整索引
+	common_parent->keys[i-1] = r_node->keys.front();
+
+	// 抹掉右兄弟的索引和指针
+	r_node->children.erase(r_node->children.begin());
+	r_node->keys.erase(r_node->keys.begin());
+
+	// 调整结点大小
+	r_node->num--;
+	l_node->num++;
+}
+
+template <typename T>
+void BPTree<T>::right_rotate(std::shared_ptr<TreeNode<T>> &l_node, std::shared_ptr<TreeNode<T>> &r_node)
+{
+	auto p = r_node;
+	auto common_parent = p->parent;
+
+	// 寻找共同的父亲
+	while(common_parent->children[0] == p)
+	{
+		p = common_parent;
+		common_parent = common_parent->parent;
+	}	
+
+	auto i = 0;
+	for(i = 0; i < common_parent->children.size(); i++)
+	{
+		if(common_parent->children[i] == p)
+		{
+			break;
+		}
+	}
+
+	auto key_tmp = common_parent->keys[i-1];
+
+	// 将借到的兄弟插入
+	r_node->children.insert(r_node->children.begin(), l_node->children.back());
+	r_node->keys.insert(r_node->keys.begin(), key_tmp);
+
+	// 调整父指针
+	r_node->children[0]->parent = r_node;
+
+	//调整索引
+	common_parent->keys[i-1] = l_node->keys.back();
+
+	// 抹掉左兄弟转移的索引和指针
+	l_node->children.pop_back();
+	l_node->keys.pop_back();
+
+	// 调整结点大小
+	r_node->num++;
+	l_node->num--;
 }
